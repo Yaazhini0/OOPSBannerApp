f3f1e7edb32a6a780574a857df2d1a0960de639a
Yaazhini0 <yaazhini.n@gmail.com>
2026-03-10 13:33:13 +0530

    added feature/UC2

---
+++ src/OopsBannerApp.java
@@ -0,0 +1,14 @@
+public class OopsBannerApp {
+    public static void main(String[] args) {
+        // uc2:
+        System.out.println("   ***   "+""+"   ***   "+""+"******   "+""+"   *****");
+        System.out.println(" **   ** "+""+" **   ** "+""+"**    ** "+""+" **");
+        System.out.println("**     **"+""+"**     **"+""+"**     **"+""+"**");
+        System.out.println("**     **"+""+"**     **"+""+"**    ** "+""+" **");
+        System.out.println("**     **"+""+"**     **"+""+"******   "+""+"  ***");
+        System.out.println("**     **"+""+"**     **"+""+"**       "+""+"     **");
+        System.out.println("**     **"+""+"**     **"+""+"**       "+""+"      **");
+        System.out.println(" **   ** "+""+" **   ** "+""+"**       "+""+"     **");
+        System.out.println("   ***   "+""+"   ***   "+""+"**       "+""+"*****");
+    }
+}
